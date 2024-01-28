2af58701a5b8690767d793ab657cee0e4f897950
Leandro Cabral <leandrocabralalmeida@gmail.com>
2024-01-28 00:38:58 -0300

    second commit

---
+++ rolling_dice/dice_visual.py
@@ -0,0 +1,31 @@
+import plotly.express as px
+
+from die import Die
+
+# Cria dois dados D6
+
+die_1 = Die()
+die_2 = Die()
+
+# Realiza alguns testes e armazena os resultados em uma lista
+results = []
+
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analisa os resultados
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
+
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualiza os resultados
+title = "Results of Rolling Two D6 Dices 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency os Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.update_layout(xaxis_dtick=1)
+fig.show()
